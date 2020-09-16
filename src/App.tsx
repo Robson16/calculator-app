da105ebe26c25532e7afa58594ff1a3dface66e5
Robson <robhenrod@gmail.com>
2020-09-16 14:31:47 -0300

    Versão antiga

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Calculator from "./components/Calculator";
+
+import "./assets/styles/global.css";
+
+function App() {
+  return (
+    <>
+      <h1>App Calculadora</h1>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
