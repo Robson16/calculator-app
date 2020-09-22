4ca8b38b62290fa0a50847b6461f0e233744e6b6
Robson <robhenrod@gmail.com>
2020-09-22 15:01:21 -0300

    Updates

---
+++ precache-manifest.be63713488a0d43f3605312198fbe9d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37360653b0f205cd762e43e81d49f3e9",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "cb57d20c6c01571159e1",
+    "url": "/calculator-app/static/css/main.70e588eb.chunk.css"
+  },
+  {
+    "revision": "4a14015bca3659c71740",
+    "url": "/calculator-app/static/js/2.16f5f2b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-app/static/js/2.16f5f2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb57d20c6c01571159e1",
+    "url": "/calculator-app/static/js/main.90bc4118.chunk.js"
+  },
+  {
+    "revision": "3f465b5e0c78badb6f74",
+    "url": "/calculator-app/static/js/runtime-main.d16fea3e.js"
+  },
+  {
+    "revision": "1ea3d76db7f7d88f2d21ac749f5ca0e4",
+    "url": "/calculator-app/static/media/mountain.1ea3d76d.jpg"
+  }
+]);
